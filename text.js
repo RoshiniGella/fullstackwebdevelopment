8f6344484b54487b64a21338cb9a7fc010326e3f
RoshiniGella <roshinigella@gmail.com>
2025-03-26 14:27:14 +0530

    eventlisteners

--- text.js
+++ text.js
@@ -1,5 +1,11 @@
-let a=6;
+let a = 6;
 console.log(a);
-const b=7;
-let c=null;
+const b = 7;
+let c = null;
 console.log(b);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+for (let n = 3, i = 0; i < n; i++) {
+    console.log("*", repeat(i));
+}
